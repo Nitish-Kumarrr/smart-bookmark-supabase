ed13e1088e9bcd17afd2748f2cc0ee5dde7a6bba
Nitish-Kumarrr <tech.nitishkumar03@gmail.com>
2026-02-20 13:27:05 +0530

    chore:remove

--- src/lib/auth-actions.ts
+++ src/lib/auth-actions.ts
@@ -28,8 +28,7 @@ export async function login(formData: FormData) {
 export async function signup(formData: FormData) {
   const supabase = await createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
+  
   const firstName = formData.get("first-name") as string;
   const lastName = formData.get("last-name") as string;
   const data = {
